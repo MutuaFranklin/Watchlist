360e5d83c6913846e8736f9e298d59cc5cf40509
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-08-07 13:28:30 +0300

    Bootstrap error handling

--- app/main/views.py
+++ app/main/views.py
@@ -1,15 +1,13 @@
 from flask import render_template,request,redirect,url_for
-from app import app
-from .requests import get_movies,get_movie,search_movie
-from .models import reviews
+from . import main
+from ..requests import get_movies,get_movie,search_movie
 from .forms import ReviewForm
+from ..models import Review
 
-Review = reviews.Review
 
 
 # Views
-
-@app.route('/')
+@main.route('/')
 def index():
     
     '''
@@ -26,12 +24,12 @@ def index():
     search_movie = request.args.get('movie_query')
 
     if search_movie:
-        return redirect(url_for('search',movie_name=search_movie))
+        return redirect(url_for('main.search',movie_name=search_movie))
     else:
         return render_template('index.html', title = title, popular = popular_movies, upcoming = upcoming_movie, now_showing = now_showing_movie )
 
 
-@app.route('/movie/<int:id>')
+@main.route('/movie/<int:id>')
 def movie(id):
 
     '''
@@ -44,7 +42,7 @@ def movie(id):
     return render_template('movie.html',title = title,movie = movie,reviews = reviews)
 
 
-@app.route('/search/<movie_name>')
+@main.route('/search/<movie_name>')
 def search(movie_name):
     '''
     View function to display the search results
@@ -56,7 +54,7 @@ def search(movie_name):
     return render_template('search.html', title = title, movies = searched_movies)
 
 
-@app.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
+@main.route('/movie/review/new/<int:id>', methods = ['GET','POST'])
 def new_review(id):
     form = ReviewForm()
     movie = get_movie(id)
---
+++ start.sh
@@ -0,0 +1,5 @@
+export MOVIE_API_KEY='a39313391038c326a4b959984d2e07d4'
+export SECRET_KEY='ad#dh$jfkdo'
+
+
+python3 manage.py server

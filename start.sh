506e87f1143dbf16d2ff3e16cefbb65bf7a62461
MutuaFranklin <franklin.mutua@student.moringaschool.com>
2021-08-12 22:37:50 +0300

    start.sh update

--- start.sh
+++ start.sh
@@ -1,8 +1,7 @@
 export MOVIE_API_KEY='a39313391038c326a4b959984d2e07d4'
 export SECRET_KEY='ad#dh$jfkdo'
 
-# export MAIL_USERNAME='franklinngumbi@gmail.com'
-# export MAIL_PASSWORD='12345'
-
+export MAIL_USERNAME='mbogiyamoringa@gmail.com'
+export MAIL_PASSWORD='Tryhackingm3bitch!'
 
 python3 manage.py server
